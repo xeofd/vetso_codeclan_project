dc2b591a88e34b04716b4df11fb4a79ff173afa7
ben <benfd@outlook.com>
2020-08-09 11:42:20 +0100

    Created PetType objects and saved them to db, 5/5 successful

--- app/console.py
+++ app/console.py
@@ -1,7 +1,23 @@
 # Import required models
-from models.classes import Pet, Owner, PetType
+from models.classes import Pet, Owner, PetType, Vet, Note
 import repositories.pet_type_repository as PTR
 import repositories.owner_repository as OR
 import repositories.pet_repository as PR
+import repositories.note_repository as NR
+import repositories.vet_repository as VR
 
 # Create data
+pet_type_1 = PetType('Dog', 'Spaniel')
+PTR.save(pet_type_1)
+
+pet_type_2 = PetType('Dog', 'Boarder Collie')
+PTR.save(pet_type_2)
+
+pet_type_3 = PetType('Cat', 'Scottish Fold')
+PTR.save(pet_type_3)
+
+pet_type_4 = PetType('Horse', 'Clydesdale')
+PTR.save(pet_type_4)
+
+pet_type_5 = PetType('Lizard', 'Geko')
+PTR.save(pet_type_5)
