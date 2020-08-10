314c4be99baa9744257e878c47d1240759306b93
ben <benfd@outlook.com>
2020-08-10 14:10:09 +0100

    5/5 TESTS PASSING - Finished tests for now. Will do more during extensions.

--- app/tests/objects_tests.py
+++ app/tests/objects_tests.py
@@ -20,6 +20,8 @@ class VetsoObjectsTesting(unittest.TestCase):
 
         # Pets
         self.pet_1 = Pet('Stuart', '20-02-20', self.owner_1, self.type_1, self.vet)
+        self.pet_2 = Pet('Holly', '20-02-20', self.owner_1, self.type_2, self.vet)
+        self.pet_3 = Pet('Bran', '20-02-20', self.owner_2, self.type_1, self.vet)
 
     # Define tests
     def test_pet_object_pet_name(self):
@@ -27,6 +29,15 @@ class VetsoObjectsTesting(unittest.TestCase):
 
     def test_pet_object_owner_name(self):
         self.assertEqual('Ben', self.pet_1.owner.first_name)
+
+    def test_pet_object_type_breed(self):
+        self.assertEqual('Turtleshell', self.pet_2.pet_type.breed)
+
+    def test_owner_object_email(self):
+        self.assertEqual('ben@ben.com', self.owner_1.email)
+
+    def test_owner_object_contact_number(self):
+        self.assertEqual('0123456781', self.owner_2.contact_number)
 
 # Allow tests to run
 
